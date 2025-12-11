7c9040fae340ef7d903e2cf7571f6d5ce43c460b
hitheshamin24 <hitheshamin423@gmail.com>
2025-12-11 19:32:40 +0530

    backend setting done

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -9,8 +9,4 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
 });
-
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS length:", process.env.EMAIL_PASS?.length);
-
 module.exports = transporter;
