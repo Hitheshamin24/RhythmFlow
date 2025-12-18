acef06594b24268bb4bd63f272bf8ffa8ae2987c
hitheshamin24 <hitheshamin423@gmail.com>
2025-12-18 19:05:44 +0530

    updated mails

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "gmail",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 module.exports = transporter;
