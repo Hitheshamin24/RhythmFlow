3fda3a60f48f391b528967a8d4c3ca5f34ef01ac
hitheshamin24 <hitheshamin423@gmail.com>
2025-12-19 14:21:03 +0530

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,31 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json())
+app.use(
+  cors({
+    origin: ["https://dncr.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("Dance Studio API is running")
-})
-app.use("/api/auth",require("./routes/authRoutes"))
-app.use("/api/students",require("./routes/studentRoutes"))
-app.use("/api/attendance",require("./routes/attendenceRoutes"))
+app.get("/", (req, res) => {
+  res.send("Dance Studio API is running");
+});
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/students", require("./routes/studentRoutes"));
+app.use("/api/attendance", require("./routes/attendenceRoutes"));
 app.use("/api/payments", require("./routes/paymentRoutes"));
 app.use("/api/finance", require("./routes/financeRoutes"));
 app.use("/api/batches", require("./routes/batchRoutes"));
 app.use("/api/settings", require("./routes/settingsRoutes"));
 app.use("/api/studio", require("./routes/studioRoutes"));
 
-const PORT=process.env.PORT||5000
-app.listen(PORT,()=>{
-    console.log(`server running on ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
